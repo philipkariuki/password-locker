3c566af1ee19a3fb7fa2aafa0a658b2abb180575
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-16 23:08:45 +0300

    Update credentials.py
    Creates function to generate random password

--- credentials.py
+++ credentials.py
@@ -25,3 +25,18 @@ class Credentials:
 
 
 	def show_credentials
+
+
+	def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+		'''
+		Function to generate an 8 character password for a credential
+		'''
+		gen_pass=''.join(random.choice(char) for _ in range(size))
+		return gen_pass
+
+
+	def randomStrongPassword(stringLength=8):
+    """Generate a random string of letters, digits and special characters """
+    password_characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(password_characters) for i in range(stringLength))
+
