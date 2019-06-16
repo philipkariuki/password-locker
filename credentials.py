60cd12b013f2e7dd9c58762bd86357f28d933e91
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-17 00:01:08 +0300

    Update credentials.py
    Adds check user name and password def

--- credentials.py
+++ credentials.py
@@ -6,37 +6,47 @@ class Credentials:
 		Class that generates new instances of user.
 	"""
 
+# defining variables
 	credentials_list = [] # Empty credentials list
 
-	def __init__(self,user_name,site_name,account_username,password):
+	
 
-		# docstring removed for simplicity
 
-			self.user_name = user_name
-			self.site_name = site_name
-			self.account_username = account_username
-			self.password = password
+def __init__(self,user_name,site_name,account_username,password):
 
-	def save_credentials(self):
+	# docstring removed for simplicity
+
+		self.user_name = user_name
+		self.site_name = site_name
+		self.account_username = account_username
+		self.password = password
+
+
+
+@classmethod
+	def check_user(cls,first_name,password):
 		'''
-		Function to save new user credentials
+		Method that checks if the name and password entered match entries in the users_list
 		'''
-		User.credentials_list.append(self)
+		current_user = ''
+		for user in User.users_list:
+			if (user.first_name == first_name and user.password == password):
+				current_user = user.first_name
+		return current_user
 
 
-	def show_credentials
+
+def save_credentials(self):
+	'''
+	Function to save new user credentials
+	'''
+	User.credentials_list.append(self)
 
 
-	def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
-		'''
-		Function to generate an 8 character password for a credential
-		'''
-		gen_pass=''.join(random.choice(char) for _ in range(size))
-		return gen_pass
 
 
-	def randomStrongPassword(stringLength=8):
-    """Generate a random string of letters, digits and special characters """
-    password_characters = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(password_characters) for i in range(stringLength))
+def randomStrongPassword(stringLength=8):
+  """Generate a random string of letters, digits and special characters """
+  password_characters = string.ascii_letters + string.digits + string.punctuation
+  return ''.join(random.choice(password_characters) for i in range(stringLength))
 
