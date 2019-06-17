827af403f77ae71cc0f7d16b34f03429b696545c
philipkariuki <49680448+philipkariuki@users.noreply.github.com>
2019-06-17 08:49:38 +0300

    Creates credentials_test

--- run.py
+++ run.py
@@ -6,43 +6,41 @@ def create_user(first_name,last_name,email,password):
 	Function to create new user
 	"""
 
-	new_user = new User(first_name,last_name,email,password)
+	new_user = User(first_name,last_name,email,password)
 	return new_user
 
 def save_user(user):
-  '''
-  Function to save user
-  '''
-  user.save_user()
+	'''
+	Function to save user
+	'''
+	user.save_user()
 
 
 def userVerify(first_name,password):
 	'''
 	Checks the existance of a user before creating credentials
 	'''
-	checks_user = Credential.check_user(first_name,password)
+	checks_user = Credentials.check_user(first_name,password)
 	return checks_user
 
 
 def generate_password():
 
-	gen_pass = Credential.genRandomStrongPassword()
+	gen_pass = Credentials.genRandomStrongPassword()
 	return gen_pass
 
 
 def create_credential(user_name,site_name,account_user_name,password):
 
-	new_credential=Credential(user_name,site_name,account_user_name,password)
+	new_credential=Credentials(user_name,site_name,account_user_name,password)
 	return new_credential
 
 def save_credential(credential):
 
-	Credential.save_credentials(credential)
+	Credentials.save_credentials(credential)
 
 
 def show_credentials(user_name):
-	return Credential.show_credentials(user_name)
+	return Credentials.show_credentials(user_name)
 
 
- # Main
- 
